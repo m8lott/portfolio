50a230b8f6afe657f0cf255040c5dd9cddbd21be
m8loot <tazorbeatsss@gmail.com>
2023-09-06 16:32:19 +0300

    add: new project

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -2,6 +2,7 @@ import project01 from "./../img/projects/project-1.jpg"
 import project02 from "./../img/projects/project-2.jpg"
 import project03 from "./../img/projects/project-3.jpg"
 import project04 from "./../img/projects/project-4.jpg"
+import project05 from "./../img/projects/project-5.jpg"
 
 const projects = [
   {title:'Dental clinic',
@@ -20,6 +21,10 @@ const projects = [
     skills:'React, Tailwind CSS',
     img:project04,
     gitHubLink:'https://github.com/m8lott/CurrencyConverter/'},
+    {title:'React Pizza',
+    skills:'ReactTS, JS, CSS',
+    img:project05,
+    gitHubLink:'https://m8lott.github.io/ReactTSPizza/'}
 ]
 
 export{projects}
